0cdc8eb8f55cb8f48818fcc52f8dd6d4dc073754
sin <xuanmingyi@gmail.com>
2021-01-11 23:24:00 +0800

    add neotree

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 [vs-dark-theme](https://github.com/jcs-elpa/vs-dark-theme)的screen
 
 ![](https://github.com/jcs-elpa/vs-dark-theme/blob/master/etc/vs-dark-theme.png)
+
+
+### go-mode.el
+
+[go-mode.el](https://github.com/dominikh/go-mode.el)
--- init.el
+++ init.el
@@ -1,5 +1,4 @@
 ;; init.el
-;; 启动之前先初始化submodule
 
 
 (add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
@@ -7,17 +6,18 @@
 (require 'init-elpa)  ;; 设置清华源
 (require 'init-theme) ;; 初始化主题vs-dark-theme
 
+;; neotree
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+;; go-mode
+(autoload 'go-mode "go-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+(add-hook 'go-mode-hook 'lsp-deferred)
+
+;; python-mode
+
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(## auto-complete)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
